7d64d031af587442f5d0ec89a7a6d4b050dfd76c
kuteken <kuteken@gmail.com>
2011-12-29 01:30:17 -0800

    new rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImHack::Application.config.secret_token = 'cf7a6c911c1c46ce37efd808b18f7576c8c55bf545b1b9b11046c84b8c544fbb072d6ab7d15ebdc6be9ee379466a777ae6e5d32ac1677d4653a885de10698e64'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ImHack::Application.config.session_store :cookie_store, key: '_im_hack_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ImHack::Application.config.session_store :active_record_store
